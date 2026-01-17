17c0bf6b8d5ee7d356164fc4f847311dc6a33e97
Emmanuel Folarin <folarin.emmanuel@outlook.com>
2026-01-17 09:32:54 +0000

    Fix: specific image hostname error by allowing all remote patterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,28 +4,8 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
+                hostname: '**',
             },
-            {
-                protocol: 'https',
-                hostname: 'japahub-34138.firebasestorage.app',
-            },
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'graph.facebook.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            },
-            {
-                protocol: 'https',
-                hostname: 'ichef.bbci.co.uk',
-            }
         ],
     },
     output: 'standalone',
